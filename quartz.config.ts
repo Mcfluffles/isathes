3dca5b24f55b7a5372360ce9ecfeb70737d77525
Alex <masnsmth42@gmail.com>
2026-01-30 18:45:13 -0600

    Quartz sync: Jan 30, 2026, 6:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "mcfluffles.github.io/isathes",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Unedited"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
